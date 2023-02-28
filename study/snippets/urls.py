b9e73173c9614f54279fc5d2b63f5cb40bc9cfcc
maksimuspiter <maksimuspiter@gmail.com>
2023-02-28 13:39:36 +0300

    add login and permission

--- study/snippets/urls.py
+++ study/snippets/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
@@ -10,7 +10,13 @@ urlpatterns = [
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
 """
-add suffix
-http://127.0.0.1:8000/snippets.json  # JSON suffix
-http://127.0.0.1:8000/snippets.api   # Browsable API suffix
+    add suffix
+    http://127.0.0.1:8000/snippets.json  # JSON suffix
+    http://127.0.0.1:8000/snippets.api   # Browsable API suffix
+"""
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
+]
+"""
+add button log in
 """
--- study/snippets/views.py
+++ study/snippets/views.py
@@ -1,19 +1,15 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.http import Http404
-
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import mixins
 from rest_framework import generics
 from django.contrib.auth.models import User
+from rest_framework import permissions
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         """ Save owner: appear new field in request """
@@ -23,6 +19,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class UserList(generics.ListAPIView):
