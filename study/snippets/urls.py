4ca391edf423bc05c69373a7093f6702a5c91644
maksimuspiter <maksimuspiter@gmail.com>
2023-02-28 12:36:12 +0300

    add suffix urls

--- study/snippets/urls.py
+++ study/snippets/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
+"""
+add suffix
+http://127.0.0.1:8000/snippets.json  # JSON suffix
+http://127.0.0.1:8000/snippets.api   # Browsable API suffix
+"""
